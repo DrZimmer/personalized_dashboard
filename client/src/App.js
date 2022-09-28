67e5a000fcb249636b0b6aa5d960e34711038a05
White Tiger <dayley.zimmermann@gmail.com>
2022-09-28 13:41:06 -0400

    added more react framework, header footer, need to figure out how i want navigation to be

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,28 @@
+import React from 'react';
 import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import NoMatch from './pages/NoMatch';
 
 function App() {
   return (
-    <h1>Hello!</h1>
+    <BrowserRouter>
+      <Routes>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+            <div className="container">
+          
+          <Route exact path="/" component={Home} />
+          {/* <Route exact path="/profile/:username" component={Profile} /> */}
+          <Route component={NoMatch} />
+        
+      </div>
+      <Footer />
+    </div>
+  </Routes>
+  </BrowserRouter>
   );
 }
 
